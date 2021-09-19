09db2b0c333c7abd52350fff2d9b9746b28c01ab
George White <me@galexite.uk>
2021-09-19 11:05:38 +0100

    Update package.json, install prettier

--- src/container.ts
+++ src/container.ts
@@ -1,34 +1,35 @@
-import * as vscode from 'vscode';
-import path = require('path');
+import * as vscode from "vscode";
+import path = require("path");
 
 export class Container {
-    private static _context: vscode.ExtensionContext;
+  private static _context: vscode.ExtensionContext;
 
-    static initialize(context: vscode.ExtensionContext) {
-        Container._context = context;
+  static initialize(context: vscode.ExtensionContext) {
+    Container._context = context;
+  }
+
+  public static get context(): vscode.ExtensionContext {
+    return Container._context;
+  }
+
+  static get workspace(): vscode.Memento {
+    return Container._context.workspaceState;
+  }
+
+  static get workspaceFolderFsPath(): string | undefined {
+    const folders = vscode.workspace.workspaceFolders;
+    if (!folders) {
+      return undefined;
     }
-
-    public static get context(): vscode.ExtensionContext {
-        return Container._context;
+    if (!folders.length) {
+      return undefined;
     }
+    return folders[0].uri.fsPath;
+  }
 
-    static get workspace(): vscode.Memento {
-        return Container._context.workspaceState;
-    }
-
-    static get workspaceFolderFsPath(): string | undefined {
-        const folders = vscode.workspace.workspaceFolders;
-        if (!folders) {
-            return undefined;
-        }
-        if (!folders.length) {
-            return undefined;
-        }
-        return folders[0].uri.fsPath;
-    }
-
-    static file(...names: string[]): vscode.Uri {
-        return vscode.Uri.file(path.join(Container._context.extensionPath, ...names));
-    }
-
+  static file(...names: string[]): vscode.Uri {
+    return vscode.Uri.file(
+      path.join(Container._context.extensionPath, ...names)
+    );
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,12 @@
-import { registerProblemMatchers } from 'bazel-stack-vscode-api';
-import * as vscode from 'vscode';
-import { CompilationDatabase } from './compilationDatabase';
-import { Container } from './container';
+import { registerProblemMatchers } from "bazel-stack-vscode-api";
+import * as vscode from "vscode";
+import { CompilationDatabase } from "./compilationDatabase";
+import { Container } from "./container";
 
 export function activate(context: vscode.ExtensionContext) {
-	Container.initialize(context);
-	registerProblemMatchers(context);
-	context.subscriptions.push(new CompilationDatabase());
+  Container.initialize(context);
+  registerProblemMatchers(context);
+  context.subscriptions.push(new CompilationDatabase());
 }
 
-export function deactivate() { }
+export function deactivate() {}
